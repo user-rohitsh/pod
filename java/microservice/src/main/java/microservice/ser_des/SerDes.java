1dd9415c9bfd19c9fd0b9e484bb388c2e7ffd4d0
rohit sharma <rohitsharma@rohits-MacBook-Air.local>
2022-08-09 15:05:20 +0530

    removed microservice

--- java/microservice/src/main/java/microservice/ser_des/SerDes.java
+++
@@ -1,8 +0,0 @@
-package microservice.ser_des;
-
-import java.nio.ByteBuffer;
-
-public interface SerDes<M> {
-    ByteBuffer toBytes(M message);
-    M fromBytes(ByteBuffer bytes);
-}
--- java/microservice/src/main/java/microservice/state_management/ApplicationState.java
+++
@@ -1,9 +0,0 @@
-package microservice.state_management;
-
-public interface ApplicationState <V> {
-    void save(V message);
-    V retrieve();
-    long position();
-    void seek(long position);
-    void rewind(long diff);
-}

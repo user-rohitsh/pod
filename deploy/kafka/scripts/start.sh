51a5d607bc9da255411a628797f4f0344315a021
rohit sharma <rohitsharma@rohits-MacBook-Air.local>
2022-09-14 13:54:28 +0530

    kafka conatiners

--- deploy/kafka/scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nohup ~/kafka/bin/zookeeper-server-start.sh ~/kafka/config/zookeeper.properties >~/start_zoo.log 2>&1 &
-sleep 20
-~/kafka/bin/kafka-server-start.sh ~/kafka/config/server.properties >~/start_kafka.log 2>&1
